cc067252ba57a8b721d3bbd8233177a0cb190005
dhui <dhui0226@gmail.com>
2021-11-26 17:51:35 -0600

    can pull currently playing song info

--- src/WebPlayback.js
+++ src/WebPlayback.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 function WebPlayback(props) {
+    const { token } = props
 
     const [player, setPlayer] = useState(undefined);
+    const [song, setSong] = useState('')
 
     useEffect(() => {
 
@@ -32,13 +34,39 @@ function WebPlayback(props) {
     
             player.connect();
         };
-    }, []);
+
+        async function getCurrentlyPlaying() {
+            const result = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+                headers: {
+                    'Authorization' : `Bearer ${token}`
+                }
+            })
+
+            return result
+        }
+
+        async function getSongInfo() {
+            const response = await getCurrentlyPlaying()
+    
+            if (response.status === 204 || response.status > 400) {
+                console.log('not playing')
+            }
+            
+            const spotSong = await response.json()
+            setSong(spotSong)
+
+            console.log('song', spotSong)
+        }
+
+        getSongInfo()
+    }, [])
 
     return (
         <>
             <div className="container">
                 <div className="main-wrapper">
-                
+                    <h1>{song.item.name}</h1>
+                    <h2>{song.item.id}</h2>
                 </div>
             </div>
         </>
